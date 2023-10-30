b47fb3c16ff5fb0ef38fa3f734cd8462d418b30d
Filipe Borges <filipborgs48@gmail.com>
2023-10-30 15:10:04 -0300

    test: add image analysis endpoint

--- __test__/integration/img-analysis.test.ts
+++ __test__/integration/img-analysis.test.ts
@@ -1,20 +1,24 @@
-import { main } from '@/index'
+import aws from 'aws-sdk'
 import { makeRequestMock } from '@/test/mocks/request'
-import { badRequest } from '@/utils'
-
+import { badRequest, ok, serverError } from '@/utils'
+import { main } from '@/index'
 
 describe('Image analyse tests suite', () => {
 
   test('It should analyse successfuly the image return the results', async () => {
     const result = await main(makeRequestMock())
-    expect(result).toEqual({})
+    expect(result).toEqual(ok('99.998% de ser do tipo Animal 99.998% de ser do tipo canino 99.998% de ser do tipo cão 99.998% de ser do tipo golden retriever 99.998% de ser do tipo mamífero 99.998% de ser do tipo animal de estimação'))
   })
 
   test('Given and empty queryString it should return status code 400', async () => {
     const request = makeRequestMock()
-    // @ts-expect-error
     delete request.queryStringParameters.imageUrl
     const result = await main(request)
     expect(result).toEqual(badRequest('An IMG is required!'))
+  })
+
+  test('Should return 500 if an unexpected error occurs', async () => {
+    const result = await main(null)
+    expect(result).toEqual(serverError(null))
   })
 })
--- src/factory.ts
+++ src/factory.ts
@@ -1,5 +1,9 @@
-import { Rekognition, Translate } from 'aws-sdk'
+import { Rekognition, Translate, config } from 'aws-sdk'
 import { Handler } from './handler'
+
+if (process.env.NODE_ENV === 'test') {
+  config.update({ region: 'us-east-1' });
+}
 
 const reko = new Rekognition()
 const translator = new Translate()
